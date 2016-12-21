700542bc6bf6943e849bd713487a4670da99b740
Yunpeng Niu <Beckid@users.noreply.github.com>
2016-12-21 21:10:00 +0800

    update settings related to database

--- src/config/config.php
+++ src/config/config.php
@@ -1,7 +1,7 @@
 <?php
-// Connect to the database server
-$db_server = "127.0.0.1";
-$db_uname = "root";
-$db_pword = "86395676";
-$db_name = "Blog";
+// Settings that is required for connecting to the database server
+define('DB_SERVER', '127.0.0.1', TRUE);
+define('DB_UNAME', 'root', TRUE);
+define('DB_PWORD', '86395676', TRUE);
+define('DB_NAME', 'Blog', TRUE);
 ?>
